1840349756d9a7852fa1af7476aa87279a6c8410
5pierre <pllieske@outlook.fr>
2024-10-24 02:38:03 +0200

    edit

--- FRONDEND/index.js
+++ FRONDEND/index.js
@@ -1,1 +1,35 @@
 //code pour frondend
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+//----------------------------------------------pour les test ----------------
+    app.get('/test', (req, res) =>{
+      res.sendFile(`${__dirname}/login.html`)   
+    })
+    app.get('/login.css', (req, res) =>{
+      res.sendFile(`${__dirname}/login.css`)   
+    })
+    app.get('/bdd.js', (req, res) =>{
+      res.sendFile(`${__dirname}/BDD.js`)   
+    })
+
+
+app.listen(port, () => {
+console.log(`Server is running on http://localhost:${port}`);
+});
+ 
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
